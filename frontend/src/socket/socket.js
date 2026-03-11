5bc52962e2220bd792a9ce03ba2279c4e1b4272d
Rakesh <rakeshreddymeda039@gmail.com>
2026-03-11 18:49:11 +0530

    Fix socket URL for production

--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("hhttps://devcollab-2iq3.onrender.com/");
+const socket = io("https://devcollab-2iq3.onrender.com/");
 
 export default socket;
