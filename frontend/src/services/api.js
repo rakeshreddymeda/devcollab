43ea308ac982c4c13475a021062d289622ba9283
Rakesh <rakeshreddymeda039@gmail.com>
2026-03-11 18:55:40 +0530

    Fix socket URL for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "https://devcollab-2iq3.onrender.com//api"
+    baseURL: "https://devcollab-2iq3.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
