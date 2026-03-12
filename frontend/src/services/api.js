10a2dcb78e7dd045af4b1bbade38068e87d077d5
Rakesh <rakeshreddymeda039@gmail.com>
2026-03-12 19:02:34 +0530

    Update server CORS settings and change API base URLs for production

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://localhost:5000/api"
+    baseURL: "https://devcollab-2iq3.onrender.com/api"
 });
 
 API.interceptors.request.use((req) => {
--- frontend/src/socket/socket.js
+++ frontend/src/socket/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-const socket = io("http://localhost:5000");
+const socket = io("https://devcollab-2iq3.onrender.com");
 
 export default socket;
